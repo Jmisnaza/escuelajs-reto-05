a91af63e4171de5bd56eb3cc5a17c19b25d88800
jeisson <jeisson.misnaza@gdx.travel>
2019-09-29 21:00:52 -0500

    Reto5

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
+//const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
 
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       const characters = response.results;
+      const next_fech = response.info.next;
+      localStorage.setItem("next_fech", next_fech)
       let output = characters.map(character => {
-        return `
+        return ` 
       <article class="Card">
         <img src="${character.image}" />
         <h2>${character.name}<span>${character.species}</span></h2>
@@ -22,11 +25,30 @@ const getData = api => {
     })
     .catch(error => console.log(error));
 }
+window.onunload = unloadPage;
 
-const loadData = () => {
-  getData(API);
+function unloadPage() {
+  localStorage.clear();
 }
+const loadData = async () => {
+  let apiC = localStorage.getItem("next_fech");
+  if (apiC !== null) {
+    await getData(apiC);
+    if (!apiC) {
+      const h2 = ` 
+          <h2>No existen mas personajes</h2>   
+        `
+      let newItem = document.createElement('section');
+      newItem.innerHTML = h2;
+      $app.appendChild(newItem);
 
+    } else {
+      await getData(apiC);
+    }
+  } else {
+    await getData(API);
+  }
+}
 const intersectionObserver = new IntersectionObserver(entries => {
   if (entries[0].isIntersecting) {
     loadData();
